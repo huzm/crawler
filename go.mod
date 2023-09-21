e319d5ab88d1cdf05d21c630d99c4a4c3efb3d86
huzhiming <huzhiming@shenqingtech.com>
2023-09-21 18:29:30 +0800

    use strings count and containns

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/geekbang/crawler
 
 go 1.21.0
+
+require (
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,33 +1,52 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 	"io"
 	"net/http"
 	"strings"
 )
 
-func main() {
-	url := "https://www.thepaper.cn/"
+func Fetch(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 
 	if err != nil {
-		fmt.Printf("fetch url error:%v", err)
-		return
+		panic(err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 		fmt.Printf("Error Status code:%v", resp.StatusCode)
-		return
 	}
-	body, err := io.ReadAll(resp.Body)
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return io.ReadAll(utf8Reader)
+}
 
-	if err != nil {
-		fmt.Printf("read content failed:%v", err)
-		return
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes,err:= r.Peek(1024)
+
+	if err != nil{
+		fmt.Println("fetch error:%v",err)
+		return unicode.UTF8
 	}
+	e,_,_ := charset.DetermineEncoding(bytes,")
+	return e
+}
+
+func main() {
+	url := "https://www.thepaper.cn/"
 	//fmt.Println("body:", string(body))
+	body, err:= Fetch(url)
+	if err !=nil{
+		panic(err)
+	}
 	numlinks := strings.Count(string(body), "<a")
 	fmt.Printf("homepage has %d links.\n", numlinks)
 	exist := strings.Contains(string(body), "疫情")
